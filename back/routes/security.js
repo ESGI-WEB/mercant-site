a1b2443b276cf16b54bbf3c77f2ddbeedb768b66
mlaurie <96569998+mlaurie@users.noreply.github.com>
2023-07-19 09:59:06 +0200

    Update security.js

--- back/routes/security.js
+++ back/routes/security.js
@@ -7,6 +7,11 @@ module.exports = function (userService) {
     const { email, password } = req.body;
     try {
       const user = await userService.findByEmail(email);
+      
+      if (!user || !user.checkPassword(password)) {
+        return res.sendStatus(401);
+      }
+      
       res.json({ token: await user.generateToken() });
     } catch (error) {
       next(error);
@@ -17,12 +22,6 @@ module.exports = function (userService) {
     const { firstname, lastname, email, password } = req.body;
 
     try {
-      const existingUser = await userService.findByEmail(email);
-
-      if (existingUser) {
-        return res.sendStatus(409);
-      }
-
       const newUser = await userService.create({ firstname, lastname, email, password });
       res.status(201).json({ token: await newUser.generateToken() });
     } catch (error) {
