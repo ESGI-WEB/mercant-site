70d2245a3f3844f4342e85e45f0b7312d88c542b
Laurie Morin <laurie20010701@gmail.com>
2023-07-19 19:07:57 +0200

    product

---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,60 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="filter">
+        <div class="name">
+            <p>Name of yacht</p>
+            <input placeholder="Enter the name of the yacht"/>
+        </div>
+        <div class="price">
+            <p>Price</p>
+
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+
+    .filter {
+        background-color: #1f2833;
+        display: flex;
+        padding: 30px;
+        gap: 100px;
+        color: white;
+    }
+
+    p {
+        font-size: 1.5rem;
+    }
+
+    input {
+        background: transparent;
+        border-bottom: 1px solid;
+        border-top: 0;
+        border-left: 0;
+        border-right: 0;
+        border-color: white;
+        width: 400px;
+        padding-bottom: 5px;
+
+        &:hover {
+            cursor: text;
+        }
+    }
+
+    ::placeholder {
+        color: white;
+        opacity: 1;
+        font-family: Mark-Thin;
+    }
+
+    .name, .price {
+        display: flex;
+        flex-direction: column;
+        gap:5px;
+    }
+
+</style>
--- front/src/views/SecuredZone.vue
+++ front/src/views/SecuredZone.vue
@@ -9,16 +9,71 @@
 
 <template>
     <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/renting">Renting</RouterLink>
-        <RouterLink to="/location">Purchase</RouterLink>
-        <RouterLink to="/order">Orders</RouterLink>
-        <template v-if="user.role === 'Administrator'">
-            <RouterLink to="/products">Products</RouterLink>
-            <RouterLink to="/user">Users</RouterLink>
-        </template>
-        <RouterLink to="/account">Account</RouterLink>
+        <div class="order">
+            <RouterLink to="/">Purchase</RouterLink>
+            <RouterLink to="/location">Renting</RouterLink>
+            <RouterLink to="/order">Orders</RouterLink>
+        </div>
+        <div class="logo">
+            YachtMasters
+        </div>
+        <div class="account">
+            <template v-if="user.role === 'Administrator'">
+                <RouterLink to="/products">Products</RouterLink>
+                <RouterLink to="/user">Users</RouterLink>
+            </template>
+            <RouterLink to="/account"><span class="material-symbols-outlined">person</span></RouterLink>
+            <a @click="logout"><span class="material-symbols-outlined">logout</span></a>
+        </div>
     </nav>
-        Connected as {{ user.email }}
-        <a @click="logout">Logout</a>
+    <RouterView/>
 </template>
+
+<style scoped>
+
+    nav {
+        height: 70px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0 30px 0 30px;
+
+        > a, div {
+            font-family: Arial;
+            font-size: 1.8rem;
+            flex: 1;
+        }
+
+        .logo {
+            text-align: center;
+            color : #18c0d6;
+        }
+    }
+
+    .account {
+        display: flex;
+        gap: 40px;
+        justify-content: flex-end;
+        align-items: center;
+
+       > a {
+           color: inherit;
+           font-family: Mark-Light;
+           text-decoration: inherit;
+       }
+    }
+
+    .order {
+        display: flex;
+        justify-content: flex-start;
+        gap: 40px;
+        align-items: center;
+
+        > a {
+            color: inherit;
+            font-family: Mark-Light;
+            text-decoration: inherit;
+        }
+    }
+
+</style>
