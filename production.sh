a7ac17a7aa06ee923e6ee341459090bbc82fea81
Nicolas W <nwadoux@myges.fr>
2023-07-27 11:49:38 +0200

    production script

---
+++ production.sh
@@ -0,0 +1,17 @@
+# Pull repository
+git pull
+
+# create networks if they doesn't exist
+docker network create payless-network
+
+# launch dockers
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
+
+# launch migrations
+docker exec server_1 node migrate.js
+
+# launch nginx for frontend
+docker build -t nicolaswadoux/merchantsite ./merchantsite/front &&
+docker stop merchant-front &&
+docker rm merchant-front &&
+docker run --name merchant-front -d -p 80:80 nicolaswadoux/merchantsite
